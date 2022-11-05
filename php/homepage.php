e9143d34e0d54dea32e12b8256561517ef580df3
Max Navarro <matsnf88@gmail.com>
2022-11-05 09:29:23 -0600

    agregar imagenes varias

--- php/homepage.php
+++ php/homepage.php
@@ -19,29 +19,33 @@
     </head>
     <body id="page-top">
         <!-- Navigation-->
+        
         <a class="menu-toggle rounded" href="#"><i class="fas fa-bars"></i></a>
         <nav id="sidebar-wrapper">
+        <div class="transbox">
             <ul class="sidebar-nav">
-                <li class="sidebar-brand"><a href="#page-top">Aqui va algo</a></li>
+                <li class="sidebar-brand"><a href="#page-top">Descargar Curriculum</a></li>
                 <li class="sidebar-nav-item"><a href="#page-top">Inicio</a></li>
                 <li class="sidebar-nav-item"><a href="#about">Acerca</a></li>
                 <li class="sidebar-nav-item"><a href="#services">Certificaciones</a></li>
                 <li class="sidebar-nav-item"><a href="#portfolio">Portfolio</a></li>
                 <li class="sidebar-nav-item"><a href="#contact">Contacto</a></li>
             </ul>
+            </div>
         </nav>
+        
 
 
         <!-- Header-->
         <header class="masthead d-flex align-items-center">
             <div class="container px-4 px-lg-5 text-center">
 
-            <div class="transbox"> <!-- background transparent --> 
+                <div class="transbox"> <!-- background transparent --> 
 
-                <h1 class="mb-1">Maximiliano Navarro Fernández</h1>
-                <h3 class="mb-5"><em>Portafolio profesional</em></h3>
-                 <!--<a class="btn btn-primary btn-xl" href="#about">Obtener Mas información</a> -->
-            </div> <!-- background transparent --> 
+                    <h1 class="mb-1">Maximiliano Navarro Fernández</h1>
+                    <h3 class="mb-5"><em>Portafolio profesional</em></h3>
+                    <!--<a class="btn btn-primary btn-xl" href="#about">Obtener Mas información</a> -->
+                </div> <!-- background transparent --> 
             </div>
         </header>
 
@@ -65,60 +69,170 @@
                 </div>
             </div>
         </section>
-        <!-- Services-->
+
+
+
+
+         <!-- About-->
+         <section class="page-section" id="about">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Experiencia Laboral</h2>
+                    <h3 class="section-subheading text-muted">Orden Cronológico</h3>
+                </div>
+                <ul class="timeline">                    
+
+                    <li class="timeline-inverted">
+                        <div class="timeline-image"><img class="rounded-circle img-fluid" src="../resources/img/7.jpg" alt="..." /></div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4>Pasantía como Programador en Coorporación CoopeAgri. </h4>
+                                <h4 class="subheading">Mayo 2016 - Agosto 2016.</h4>
+                            </div>
+                            <div class="timeline-body"><p class="text-muted">•Creación de un sistema (Java, Oracle/PL) estadístico y de recolección de datos de estaciones meteorológicas para el sector Agro.</p>
+                                <p class="text-muted">• Elaboración de un sitio web para la empresa de acuerdo a los requerimientos solicitados por la Gerencia</p>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="timeline-image"><img class="rounded-circle img-fluid" src="../resources/img/8.jpg" alt="..." /></div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4>Asistente Administrativo Grupo Historia.</h4>
+                                <h4 class="subheading">Septiembre 2016 - Noviembre 2017</h4>
+                            </div>
+                            <div class="timeline-body"><p class="text-muted">• Mantenimiento del sistema SAP Business ERP.</p>
+                                <p class="text-muted">• Ingresar nuevos clientes al sistema.</p>
+                                <p class="text-muted">• Programar y coordinar reuniones para los gerentes.</p>
+                                <p class="text-muted">• Gestionar los informes de viajes y gastos de los miembros del equipo.</p>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="timeline-inverted">
+                        <div class="timeline-image"><img class="rounded-circle img-fluid" src="../resources/img/6.jpg" alt="..." /></div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4>Asistente de Contabilidad DITESA. </h4>
+                                <h4 class="subheading">Enero 2018 - Febrero 2019.</h4>
+                            </div>
+                            <div class="timeline-body"><p class="text-muted">• Mantenimiento del sistema SAP Business ERP.</p>
+                                <p class="text-muted">• Elaboración de informes mensuales para supervisores y gerentes.</p>
+                                <p class="text-muted">• Atención al cliente y autorización de facturación según análisis crediticio de los clientes.</p>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="timeline-image"><img class="rounded-circle img-fluid" src="../resources/img/5.1.jpg" alt="..." /></div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4>Técnico Ministerio de Justicia y Paz. </h4>
+                                <h4 class="subheading">Feb 2019 - Actualidad</h4>
+                            </div>
+                            <div class="timeline-body"><p class="text-muted">• Ingresar los datos de los reclusos en un sistema bajo la responsabilidad de la Administración Nacional Penitenciaria.</p>
+                                <p class="text-muted">• Elaborar informes estadísticos con los datos aportados.</p>
+                                <p class="text-muted">• Hacer presentaciones e infografías para los miembros del Consejo Técnico.</p>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="timeline-inverted">
+                        <div class="timeline-image">
+                            <h4>
+                                
+                                <br />
+                                Continuará
+                                <br />
+                                
+                            </h4>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </section>
+
+
+
+
+        <!-- Services
+        <div class="transbox"> -->
         <section class="content-section bg-primary text-white text-center" id="services">
+        
             <div class="container px-4 px-lg-5">
                 <div class="content-section-heading">
                    <!-- <h3 class="text-secondary mb-0">Experiencia</h3> -->
-                    <h2 class="mb-5">Experiencia Laboral</h2>
+                    <h2 class="mb-5">Cursos y Certificaciones</h2>
                 </div>
 
                 <div class="row gx-4 gx-lg-5">
                     <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
-                        <span class="service-icon rounded-circle mx-auto mb-3"><i class="icon-chart"></i></span>
-                        <h4><strong>Tecnico 1 Ministerio de Justicia y Paz. Feb 2019 - Actualidad</strong></h4>
+                        <span class="service-icon rounded-circle mx-auto mb-3"><i class="icon-share"></i></span>
+                        <h4><strong>MICROSOFT - AZ-900 Azure Fundamentals 2022.</strong></h4>
                         <p class="text-faded mb-0">
-                        <p class="text-faded mb-0">• Ingresar los datos de los reclusos en un sistema bajo la responsabilidad de la Administración Nacional Penitenciaria.</p>	
-                        <p class="text-faded mb-0">• Elaborar informes estadísticos en base a los datos aportados.</p>
-                        <p class="text-faded mb-0">• Hacer presentaciones e infografías para los miembros de la junta.</p>
+                       
+                       
                        
 
                     </div>
                     
                     <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
-                        <span class="service-icon rounded-circle mx-auto mb-3"><i class="icon-calculator"></i></span>
-                        <h4><strong>Asistente de Contabilidad DITESA Enero 2018 - Febrero 2019.</strong></h4>
-                        <p class="text-faded mb-0">• Mantenimiento del sistema SAP Business ERP.</p>	
-                        <p class="text-faded mb-0">• Elaboración de informes mensuales para supervisores y gerentes.</p>
-                        <p class="text-faded mb-0">• Atención y atención al cliente.</p>
-                        <p class="text-faded mb-0">• Autorización de facturación según análisis crediticio de los clientes.</p>
+                        <span class="service-icon rounded-circle mx-auto mb-3"><i class="icon-calendar"></i></span>
+                        <h4><strong>SCRUM STUDY - Scrum Fundamentals (SFC) 2022.</strong></h4>
+                       
                     </div>
 
                     <div class="col-lg-3 col-md-6 mb-5 mb-md-0">
-                        <span class="service-icon rounded-circle mx-auto mb-3"><i class="icon-folder-alt"></i></span>
-                        <h4><strong>Asistente Administrativo Septiembre 2016 - Noviembre 2017</strong></h4>
+                        <span class="service-icon rounded-circle mx-auto mb-3"><i class="icon-social-github"></i></span>
+                        <h4><strong>UDEMY - Curso de GIT / GitHub 2022</strong></h4>
                          <!--<p class="text-faded mb-0">
                             Millions of users
                            <i class="fas fa-heart"></i>
                             Start Bootstrap!
-                        </p>-->
+                        </p>
 
                         <p class="text-faded mb-0">• Mantenimiento del sistema SAP Business ERP.</p>	
                         <p class="text-faded mb-0">• Entrada de nuevos clientes al sistema.</p>
                         <p class="text-faded mb-0">• Programar y coordinar reuniones para los gerentes.</p>
-                        <p class="text-faded mb-0">• Gestionar los informes de viajes y gastos de los miembros del equipo.</p>
+                        <p class="text-faded mb-0">• Gestionar los informes de viajes y gastos de los miembros del equipo.</p>  -->
                     </div>
 
                     <div class="col-lg-3 col-md-6">
-                        <span class="service-icon rounded-circle mx-auto mb-3"><i class="icon-graph"></i></span>
-                        <h4><strong>Pasantía de Programador en Coorporación Coopeagri Mayo 2016 - Agosto 2016</strong></h4>
-                        <p class="text-faded mb-0">• Creación de un sistema (Java, Oracle/PL) estadístico y de recolección de datos de estaciones meteorológicas para el sector Agro.</p>
-                        <p class="text-faded mb-0">• Elaboración de un sitio web para la empresa</p>
+                        <span class="service-icon rounded-circle mx-auto mb-3"><i class="icon-screen-tablet"></i></span>
+                        <h4><strong>MICITT CECI - Desarrollo de Aplicaciones Android 2021.</strong></h4>
+                       
+                    </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <span class="service-icon rounded-circle mx-auto mb-3"><i class="icon-user-following"></i></span>
+                        <h4><strong>FATLA - Curso Presencia Digital e Imagen Corporativa 2020.</strong></h4>
+                        
                     </div>
                     
+                    <div class="col-lg-3 col-md-6">
+                        <span class="service-icon rounded-circle mx-auto mb-3"><i class="icon-settings"></i></span>
+                        <h4><strong>	INA - Técnico Reparación de Computadoras 2020</strong></h4>
+                        
+                    </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <span class="service-icon rounded-circle mx-auto mb-3"><i class="icon-social-google"></i></span>
+                        <h4><strong>GOOGLE - Curso de Cloud Computing 2019</strong></h4>
+                        
+                    </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <span class="service-icon rounded-circle mx-auto mb-3"><i class="icon-social-google"></i></span>
+                        <h4><strong>GOOGLE - Curso Desarrollo de Apps Móviles 2019</strong></h4>
+                        
+                    </div>
+
                 </div>
             </div>
+            
         </section>
+        </div>
         <!-- Callout
         <section class="callout">
             <div class="container px-4 px-lg-5 text-center">
@@ -212,7 +326,7 @@
                                 <div class="portfolio-hover">
                                     <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                                 </div>
-                                <img class="img-fluid" src="../resources/img/img1.jpg" alt="..." />
+                                <img class="img-fluid" src="../resources/img/notion.png" alt="..." />
                             </a>
                             <div class="portfolio-caption">
                                 <div class="portfolio-caption-heading">Threads</div>
@@ -517,14 +631,14 @@
 
 
 
-        <!-- Call to Action-->
+        <!-- Call to Action
         <section class="content-section bg-primary text-white">
             <div class="container px-4 px-lg-5 text-center">
                 <h2 class="mb-4">The buttons below are impossible to resist...</h2>
                 <a class="btn btn-xl btn-light me-4" href="#!">Click Me!</a>
                 <a class="btn btn-xl btn-dark" href="#!">Look at Me!</a>
             </div>
-        </section>
+        </section>  -->
 
 
         <!-- Map
@@ -533,6 +647,78 @@
             <br />
             <small><a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;aq=0&amp;oq=twitter&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;t=m&amp;z=15&amp;iwloc=A"></a></small>
         </div> -->
+
+
+
+
+                
+        <!-- Contact-->
+        <div class="transbox">
+        <section class="page-section" id="contact">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Contact Us</h2>
+                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                </div>
+                <!-- * * * * * * * * * * * * * * *-->
+                <!-- * * SB Forms Contact Form * *-->
+                <!-- * * * * * * * * * * * * * * *-->
+                <!-- This form is pre-integrated with SB Forms.-->
+                <!-- To make this form functional, sign up at-->
+                <!-- https://startbootstrap.com/solution/contact-forms-->
+                <!-- to get an API token!-->
+                <form id="contactForm" data-sb-form-api-token="API_TOKEN">
+                    <div class="row align-items-stretch mb-5">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <!-- Name input-->
+                                <input class="form-control" id="name" type="text" placeholder="Your Name *" data-sb-validations="required" />
+                                <div class="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
+                            </div>
+                            <div class="form-group">
+                                <!-- Email address input-->
+                                <input class="form-control" id="email" type="email" placeholder="Your Email *" data-sb-validations="required,email" />
+                                <div class="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
+                                <div class="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
+                            </div>
+                            <div class="form-group mb-md-0">
+                                <!-- Phone number input-->
+                                <input class="form-control" id="phone" type="tel" placeholder="Your Phone *" data-sb-validations="required" />
+                                <div class="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group form-group-textarea mb-md-0">
+                                <!-- Message input-->
+                                <textarea class="form-control" id="message" placeholder="Your Message *" data-sb-validations="required"></textarea>
+                                <div class="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Submit success message-->
+                    <!---->
+                    <!-- This is what your users will see when the form-->
+                    <!-- has successfully submitted-->
+                    <div class="d-none" id="submitSuccessMessage">
+                        <div class="text-center text-white mb-3">
+                            <div class="fw-bolder">Form submission successful!</div>
+                            To activate this form, sign up at
+                            <br />
+                            <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
+                        </div>
+                    </div>
+                    <!-- Submit error message-->
+                    <!---->
+                    <!-- This is what your users will see when there is-->
+                    <!-- an error submitting the form-->
+                    <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3">Error sending message!</div></div>
+                    <!-- Submit Button-->
+                    <div class="text-center"><button class="btn btn-primary btn-xl text-uppercase disabled" id="submitButton" type="submit">Send Message</button></div>
+                </form>
+            </div>
+        </section>
+        </div>
+
 
 
         <!-- Footer-->
