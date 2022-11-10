598164279df73e6bf7620df382b503dfea25b110
Max Navarro <matsnf88@gmail.com>
2022-11-10 14:00:44 -0600

    agregar codigo para establecer la conexion con la DB crudsem5

--- php/conexion.php
+++ php/conexion.php
@@ -1,2 +1,15 @@
+<?php  
+	$servidor = "mysql:dbname=crudsem5; host=127.0.0.1; port=3307"; // localhost     127.0.0.1
+	$usuario = "root";
+	$password = "";
 
+	try {
+		$bd = new PDO($servidor,$usuario, $password,			
+			array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+		);
+	} catch (Exception $e) {
+		echo "Error de conexión ".$e->getMessage();
+	}
+
+?>
 
