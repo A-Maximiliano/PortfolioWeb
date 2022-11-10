da4f2ac42ed0b9548d8f4895c42cf45873d3f4f3
Max Navarro <matsnf88@gmail.com>
2022-11-10 14:06:28 -0600

    agregar codigo para validar las credenciales del usuario

---
+++ php/revisar.php
@@ -0,0 +1,18 @@
+<?php 
+	session_start();
+	include_once 'conexion.php';
+	$usuario = $_POST['usuario'];
+	$contrasena = $_POST['clave'];
+	$sentencia = $bd->prepare('select * from t_cliente where 
+								Nombre = ? and Telefono = ?;');
+	$sentencia->execute([$usuario, $contrasena]);
+	$datos = $sentencia->fetch(PDO::FETCH_OBJ);
+	//print_r($datos);
+
+	if ($datos === FALSE) {
+		header('Location: login.php');
+	}elseif($sentencia->rowCount() == 1){
+		$_SESSION['nombre'] = $datos->nombre_usu;
+		header('Location: homepage.php');
+	}
+?>
